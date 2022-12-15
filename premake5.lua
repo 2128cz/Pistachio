b622d7850060a30adf9e968a5c4dcba9922ae030
lrc <571244399@qq.com>
2022-12-15 12:55:38 +0800

    加入项目自动化构建程序

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Pistachio"
+	architecture "x64"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pistachio"
+	location "PistachioEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/".. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/".. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CHIO_PLANTFORM_WINDOWS",
+			"CHIO_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CHIO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CHIO_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "CHIO_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+
+	language "C++"
+
+	targetdir ("bin/".. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/".. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"Pistachio/src"
+	}
+
+	links
+	{
+		"Pistachio"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CHIO_PLANTFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CHIO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CHIO_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "CHIO_DIST"
+		symbols "On"
